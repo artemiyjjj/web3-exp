f58ea52ce7b89fb67dea6da0f817a8742ea57d87
artemioJjJ <artemiyromanoff@gmail.com>
2023-01-14 14:07:19 +0300

    Remove useless classes and files

--- src/main/java/com/example/back/servlets/HelloServlet.java
+++ src/main/java/com/example/back/servlets/HelloServlet.java
@@ -2,16 +2,12 @@ package com.example.back.servlets;
 
 import java.io.*;
 
-import com.example.back.beans.Bean;
-import jakarta.ejb.EJB;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
-    @EJB
-    private Bean bean;
 
     public void init() {
         message = "Hello World!";
@@ -24,7 +20,7 @@ public class HelloServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
-        out.println("<h1>" + bean.hello() + "</h1>");
+        out.println("<h1>" + "</h1>");
         out.println("</body></html>");
     }
 
